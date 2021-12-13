04ef07677e6305d8252344cce61bac85babafb62
CroaBeast <66433837+CroaBeast@users.noreply.github.com>
2021-12-12 22:23:38 -0600

    Create NewActionBar.java

---
+++ general_classes/objects/NewActionBar.java
@@ -0,0 +1,14 @@
+package general_classes.objects;
+
+import general_classes.interfaces.*;
+import net.md_5.bungee.api.*;
+import net.md_5.bungee.api.chat.*;
+import org.bukkit.entity.Player;
+
+public class ActBar17 implements ActionBar {
+
+    @Override
+    public void send(Player player, String message) {
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+    }
+}
